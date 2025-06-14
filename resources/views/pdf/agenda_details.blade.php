b8cb1fd592954b8350b6b90083d699fbfd59551b
Erik Wisnu <erikwnugroho@gmail.com>
2025-06-14 16:40:29 +0700

    update 14_juni_25 2

--- resources/views/pdf/agenda_details.blade.php
+++ resources/views/pdf/agenda_details.blade.php
@@ -79,10 +79,10 @@
                         @foreach ($details as $index => $detail)
                             <tr>
                                 <td>{{ $i++ }}</td>
-                                <td>{{ $detail->no_surat }}</td>
+                                <td>{{ $detail->no_surat ?? ''  }}</td>
                                 <td>{{ $detail->dari }} / {{ \Carbon\Carbon::parse($detail->tanggal_masuk)->locale('id')->translatedFormat('d F Y') }}</td>
-                                <td>{{ $detail->perihal }}</td>
-                                <td>{{ $detail->usulan_keputusan }}</td>
+                                <td>{{ $detail->perihal ?? '' }}</td>
+                                <td>{{ $detail->usulan_keputusan ?? ''  }}</td>
                                 <td>{{ $detail->agendaKeterangan->name ?? '-' }}</td>
                             </tr>
                         @endforeach
